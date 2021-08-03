f306a4f8cb8e64eca512d4354f7689fc5dafd315
Bob <bkane90@gmail.com>
2021-08-03 17:24:06 -0400

    built js functionality

---
+++ app.js
@@ -0,0 +1,75 @@
+const song = document.querySelector('.song');
+const play = document.querySelector('.play');
+const outline = document.querySelector('.moving-outline circle');
+const video = document.querySelector('.vid-container video');
+
+//Sounds
+const sounds = document.querySelectorAll('.sound-picker button');
+
+//Time Display
+const timeDisplay = document.querySelector('.time-display');
+const timeSelect = document.querySelectorAll('.time-select button');
+const outlineLength = outline.getTotalLength();
+
+//Duration
+let fakeDuration = 600;
+
+outline.style.strokeDasharray = outlineLength;
+outline.style.strokeDashoffset = outlineLength;
+
+//Select Sound
+sounds.forEach(sound => {
+    sound.addEventListener("click", function () {
+        song.src = this.getAttribute('data-sound');
+        video.src = this.getAttribute('data-video');
+        checkPlaying(song);
+    })
+})
+
+//Play Sound
+play.addEventListener("click", () => {
+    checkPlaying(song);
+});
+
+//Select Sound Duration
+timeSelect.forEach(option => {
+    option.addEventListener("click", function () {
+        fakeDuration = this.getAttribute("data-time");
+        timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
+    });
+});
+
+//Toggle Sound
+const checkPlaying = (song) => {
+    if (song.paused) {
+        song.play();
+        video.play();
+        play.src = './svg/pause.svg';
+    } else {
+        song.pause();
+        video.pause();
+        play.src = './svg/play.svg';
+    }
+};
+
+//Animations
+song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+
+    //Time Animation
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+
+    //Circle animation
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+
+    if (currentTime >= fakeDuration) {
+        song.pause();
+        song.currentTime = 0;
+        play.src = './svg/play.svg';
+        video.pause();
+    };
+};
