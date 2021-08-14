041a992030d62a700aa761e86f2df76cf53997c6
Bob <bkane90@gmail.com>
2021-08-13 20:48:56 -0400

    edit styling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const song = document.querySelector('.song');
 const play = document.querySelector('.play');
+// const replay = document.querySelector('.replay');
 const outline = document.querySelector('.moving-outline circle');
 const video = document.querySelector('.vid-container video');
 
@@ -11,11 +12,12 @@ const timeDisplay = document.querySelector('.time-display');
 const timeSelect = document.querySelectorAll('.time-select button');
 const outlineLength = outline.getTotalLength();
 
-//Duration
-let fakeDuration = 600;
+//Set Duration
+let duration = 600;
 
 outline.style.strokeDasharray = outlineLength;
 outline.style.strokeDashoffset = outlineLength;
+timeDisplay.textContent = `${Math.floor(duration / 60)}:${("0" + Math.floor(duration % 60))}`
 
 //Select Sound
 sounds.forEach(sound => {
@@ -26,16 +28,28 @@ sounds.forEach(sound => {
     })
 })
 
-//Play Sound
+//Play Song
 play.addEventListener("click", () => {
     checkPlaying(song);
 });
 
-//Select Sound Duration
+//Replay Song
+// replay.addEventListener("click", () => {
+//     restartSong(song);
+// })
+
+// const restartSong = (song) => {
+//     let currentTime = song.currentTime;
+//     if (song.currentTime == 0) {
+//         console.log("Namaste")
+//     }
+// }
+
+//Select Duration
 timeSelect.forEach(option => {
     option.addEventListener("click", function () {
-        fakeDuration = this.getAttribute("data-time");
-        timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${("0" + Math.floor(fakeDuration % 60))}`;
+        duration = this.getAttribute("data-time");
+        timeDisplay.textContent = `${Math.floor(duration / 60)}:${("0" + Math.floor(duration % 60))}`;
     });
 });
 
@@ -55,7 +69,7 @@ const checkPlaying = (song) => {
 //Animations
 song.ontimeupdate = () => {
     let currentTime = song.currentTime;
-    let elapsed = fakeDuration - currentTime;
+    let elapsed = duration - currentTime;
     let seconds = ("0" + Math.floor(elapsed % 60)).slice(-2);
     let minutes = Math.floor(elapsed / 60);
 
@@ -63,10 +77,10 @@ song.ontimeupdate = () => {
     timeDisplay.textContent = `${minutes}:${seconds}`;
 
     //Circle animation
-    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    let progress = outlineLength - (currentTime / duration) * outlineLength;
     outline.style.strokeDashoffset = progress;
 
-    if (currentTime >= fakeDuration) {
+    if (currentTime >= duration) {
         song.pause();
         song.currentTime = 0;
         play.src = './svg/play.svg';
